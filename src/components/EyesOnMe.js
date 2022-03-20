4b25f72f75e6dd76be452dda17e7f6ef037bd50c
zahraa7 <zahraa.knass@hotmail.com>
2022-03-20 17:57:58 +0200

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+    return (
+      <button
+        onFocus={() => console.log(`Good!`)}
+        onBlur={() => console.log(`Hey! Eyes on me!`)}
+      >
+        Eyes on me
+      </button>
+    );
+  }
+  
+  export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
-// Code Keypad Component Here
+function Keypad() {
+    return (
+      <input
+        type="password"
+        onChange={() => {
+          console.log("Entering password...");
+        }}
+      ></input>
+    );
+  }
+  
+  export default Keypad;
